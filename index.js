24cea6078954f9c3f4afdfaa146063c65cafd5bd
AliElgohary <alielgohary35@gmail.com>
2023-06-27 23:18:13 +0300

    Fixing register and login

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import { router as userRouter } from './routes/login.js';
 import { router as categoriesRouter } from './routes/category.js';
 import { router as productRouter} from "./routes/products.js";
@@ -12,7 +13,6 @@ app.use("/categories", categoriesRouter);
 app.use("/products", productRouter);
 
 
-dotenv.config();
 
 const port = process.env.PORT;
 app.listen(port || 8080, () =>
